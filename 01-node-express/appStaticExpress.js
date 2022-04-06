4ea1aa3edf1f71cf464f5b77b7fad94ddcea5734
emuller1996 <stefano.muller@hotmail.com>
2022-04-05 21:31:40 -0500

    App Static Express

--- 01-node-express/appStaticExpress.js
+++ 01-node-express/appStaticExpress.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.get('/',(req,res) => {
-  res.send('./public/index.html');
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.get('/servicios',(req,res) => {
-  res.send('public/servicios.html',);
+  res.sendFile(path.resolve(__dirname,'public/servicios.html'));
 });
 
 
